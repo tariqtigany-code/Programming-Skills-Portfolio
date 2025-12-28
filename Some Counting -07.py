9326824711fdc9a65716dce3f1b0cdbb271420b9
tariqtigany-code <tariqtigany@gmail.com>
2025-12-29 02:38:03 +0300

    Complete Exercise 07 : Sime counting
    Iexecute five different for loops to perform various counting tasks (upward, downward, and with different increments/decrements) as specified in the instructions

---
+++ Some Counting -07.py
@@ -0,0 +1,27 @@
+# loop that counts from 0 to 50 in increments of 1
+print("Counting start from 0 to 50 :")
+for num in range(0 ,51,1): 
+    print(num)
+
+#loop that counts from 50 to 0 in decrements of 1
+print("Counting start from 50 to 0 :")
+for num in range(50 ,-1,-1): 
+    print(num)
+
+#loop that counts from 30 to 50 in increments of 1
+print("Counting start from 30 to 50 :")
+for num in range(30 ,51,1): 
+    print(num)
+
+#loop that counts from 50 to 10 in decrements of 2
+print("Counting start from 50 to 10 in decrements od 2 :")
+
+for num in range(50 ,9,-2): 
+    print(num)
+
+#loop that counts from 100 to 200 in increments of 5
+print("Counting start from 100 to 200 in increments of 5 :")
+
+for num in range(100 ,201,5): 
+    print(num)  
+
