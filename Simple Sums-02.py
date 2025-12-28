59a2c719c80744d0ca81aa2aabcdfe72a398951e
tariqtigany-code <tariqtigany@gmail.com>
2025-12-28 18:12:46 +0300

    Complete the Exercise 02 :  Simple Sums
    Solved to declare two integer variables (8 and 10 ) and calcluate sum  of them  and store in thirt variable  called calc_sum and printed the result of variable calc_sum

---
+++ Simple Sums-02.py
@@ -0,0 +1,9 @@
+#Assigning Numbers in variables
+number1 = 8 
+number2 = 10 
+#Calculate the sum of two variable 
+calc_sum = number1 + number2 
+
+#print the output sum of two variable 
+print(calc_sum)
+
