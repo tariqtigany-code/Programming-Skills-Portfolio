3127ba7d80e8180242a70f3832543dba18cee33c
tariqtigany-code <tariqtigany@gmail.com>
2025-12-28 16:48:42 +0300

    Complete Exercise 01 : Coding Is Cool
    solved to using variables and string concatenation

---
+++ Coding Is Cool-01.py
@@ -0,0 +1,12 @@
+
+#Assigning words in variables 
+word1 ="Coding"
+word2="is"
+word3="Cool"
+
+# Use string concatenation to combine the variables
+combine= word1 + " " + word2 + " " + word3 
+
+#print the phrase
+
+print(combine)
