11bd222a87762d88c4faaff65fbbd4436f7b4a0c
tariqtigany-code <tariqtigany@gmail.com>
2025-12-29 00:31:48 +0300

    Complelte Exercise 04 : Prinitive Quiz
    Executed a quiz program that ask of 10 the capitals of European contries . I used IF -ELSE statements to handle each question individually and provide immediate feedvack . I also used strip() and lower() functions to remove space and ignore capitalization  . Ensuring that ansewer such as "paris" or "Paris" or "PARIS" are will be accepted

---
+++ Primitive Quiz-04.py
@@ -0,0 +1,75 @@
+# question 1 
+Answer1 =input("What is the capital of France?") # taking input from user
+if Answer1.strip().lower() =="paris": # strip() removes extra spaces and .lower() ignores capitalization
+    print("Answer is Correct ") # feedback for correct answer
+else :
+    print("Answer is Wrong ") # feedback for wrong answer
+
+# question 2
+Answer2 =input("What is the capital of Germany?") # taking input from user
+if Answer2.strip().lower() =="berlin": # strip() removes extra spaces and .lower() ignores capitalization
+    print("Answer is Correct ") # feedback for correct answer
+else :
+    print("Answer is Wrong ") # feedback for wrong answer
+
+# question 3
+Answer3 =input("What is the capital of Italy?") # taking input from user
+if Answer3.strip().lower() =="rome": # strip() removes extra spaces and .lower() ignores capitalization
+    print("Answer is Correct ") # feedback for correct answer
+else :
+    print("Answer is Wrong ") # feedback for wrong  answer
+
+# question 4
+Answer4 =input("What is the capital of Spain?") # taking input from user
+if Answer4.strip().lower() =="madrid": # strip() removes extra spaces and .lower() ignores capitalization
+    print("Answer is Correct ") # feedback for correct answer
+else :
+    print("Answer is Wrong ") # feedback for wrong answer
+
+# question 5
+Answer5 =input("What is the capital of United Kingdom?") # taking input from user
+if Answer5.strip().lower() =="london": # strip() removes extra spaces and .lower() ignores capitalization
+    print("Answer is Correct ") # feedback for correct answer
+else :
+    print("Answer is Wrong ") # feedback for wrong answer
+
+
+# question 6
+Answer6 =input("What is the capital of Austria?") # taking input from user
+if Answer6.strip().lower() =="canberra": # strip() removes extra spaces and .lower() ignores capitalization
+    print("Answer is Correct ") # feedback for correct answer
+else :
+    print("Answer is Wrong ") # feedback for wrong answer
+
+
+# question 7
+Answer7 =input("What is the capital of Belgium?") # taking input from user
+if Answer7.strip().lower() =="brussels": # strip() removes extra spaces and the lower() ignores capitalization
+    print("Answer is Correct ") # feedback for correct answer
+else : 
+    print("Answer is Wrong ")  # feedback for wrong answer
+
+# question 8
+Answer8 =input("What is the capital of Greece?") # taking input from user
+if Answer8.strip().lower() =="athens":# strip() removes extra spaces and the lower() ignores capitalization
+    print("Answer is Correct ") # feedback for correct answer
+else :
+    print("Answer is Wrong ") # feedback for wrong answer
+
+# question 9
+Answer9 =input("What is the capital of Portugal ?") # taking input from user
+if Answer9.strip().lower() =="lisbon":# strip() removes extra spaces and the lower() ignores capitalization
+    print("Answer is Correct ") # feedback for correct answer
+else :
+    print("Answer is Wrong ") # feedback for wrong  answer
+
+# question 10
+Answer10 =input("What is the capital of Ireland?") # taking input from user
+if Answer10.strip().lower() =="dublin":# strip() removes extra spaces and the lower() ignores capitalization
+    print("Answer is Correct ")#feedback for correct answer
+else :
+    print("Answer is Wrong ")#feedback for wrong answer
+
+
+
+
