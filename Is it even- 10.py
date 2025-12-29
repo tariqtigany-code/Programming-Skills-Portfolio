800aa49fed0ca65404e64957be7918efccac1dc6
tariqtigany-code <tariqtigany@gmail.com>
2025-12-29 04:10:02 +0300

    Completed Exercise 10: Is it even?
    I developed a program that determines if a number is even or odd. The logic is encapsulated in a function that returns a message, which is then printed by the main function as required

---
+++ Is it even- 10.py
@@ -0,0 +1,20 @@
+#Function to Check if a number is even or odd 
+def check(number): 
+    if number % 2 == 0 :
+        return "The number is even "
+    else :
+        return "The number is odd"
+# Main function to handle user input and printing 
+def main():
+    #Ask the user for a number 
+    user=int(input("Enter a number ti check if it is even or odd : "))
+
+    # Pass the number to the function and get the result
+    message=check(user)
+     #print the result 
+    print(message)
+
+    #start the program 
+if __name__ == "__main__":
+    main()    
+
